f06947967d21489460d3f6bd003872ee4dba63ce
MrCharlss <camachotcarlos@gmail.com>
2018-09-14 10:09:40 -0500

    No convierte tdv de otra base a binario...

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -1,15 +1,24 @@
 class Converter {
-  converter(arr, from, to) {
+  convert(arr, from, to) {
     this.arr = arr;
     this.from = from;
     this.to = to;
+    let convertido = [];
+    let valorBaseAnterior = 0;
     let exp = this.arr.length - 1;
     for (let i = 0; i < this.arr.length; i++) {
-      let fromVal = this.from.toString() + "^" + (exp - i);
-      console.log(fromVal);
+      let fromTotal = this.arr[i] * Math.pow(this.from, exp - i);
+      valorBaseAnterior = valorBaseAnterior + fromTotal;
     }
+    console.log(valorBaseAnterior);
+    for (let i = 0; i < valorBaseAnterior.toString().length; i++) {
+      convertido.push(parseInt(valorBaseAnterior.toString()[i]));
+    }
+    return convertido;
   }
 }
 
-let prueba = new Converter();
-prueba.converter([1, 2, 3, 4, 5], 2, 3);
+//let prueba = new Converter();
+//prueba.convert([5], 10, 2);
+
+export default Converter;
